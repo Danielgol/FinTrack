0da4fe39d307e9ad64092e489d1255563f74cbd2
Daniel Rosa <dan.rosa10@gmail.com>
2022-03-07 13:21:13 -0300

    upando servidor express

---
+++ backend/src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const exampleController = require('../controllers/ExampleController')
+
+// Attach each endpoint to a specific function, on this case functions from controller
+router.get('/example', exampleController.list)
+router.get('/example/:index', exampleController.retrieve)
+router.post('/example', exampleController.create)
+router.put('/example/:index', exampleController.update)
+router.delete('/example/:index', exampleController.delete)
+
+module.exports = router
