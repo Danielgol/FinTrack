530d41cb27f1477c15d14e38b9d00190134bdf12
João Victor Cavalcanti Teixeira de Assis <joao.ctassis@gmail.com>
2022-03-24 14:32:49 -0300

    navbar responsiva na tela de login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
-import { FormloginComponent } from './components/formlogin/formlogin.component';
-import { FormregisterComponent } from './components/formregister/formregister.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -15,8 +13,6 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     IndexComponent,
     LoginComponent,
-    FormloginComponent,
-    FormregisterComponent,
     HomeComponent
   ],
   imports: [
