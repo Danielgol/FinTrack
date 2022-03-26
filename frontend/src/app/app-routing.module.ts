a276ddc9bccdf9eb25ed49755cfb6fa56bf82aae
João Victor Cavalcanti Teixeira de Assis <joao.ctassis@gmail.com>
2022-03-26 13:14:47 -0300

    Correções visuais e navegação de páginas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
   {path: "", component: IndexComponent},
   {path: "login", component: LoginComponent},
-  {path: "home", component: HomeComponent}
+  {path: "home", component: HomeComponent},
+  {path: "register", component: RegisterComponent}
 ];
 
 @NgModule({
