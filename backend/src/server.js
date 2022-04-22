bec8ff8ec27051aad2064d8874b7426167d45bc6
Daniel Rosa <dan.rosa10@gmail.com>
2022-04-22 16:13:24 -0300

    conectando to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const cors = require('cors')
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
-const routes = require('./routes');  // Since the file is index.js we don't need to specify
+const routes = require('./routes');
 
 /* https://celke.com.br/artigo/como-permitir-acesso-a-api-com-cors-parte-7
 app.use((req, res, next) => {
@@ -13,17 +14,26 @@ app.use((req, res, next) => {
     app.use(cors());
     next();
 });
-*/
 
 app.use((req, res, next) => {
     //console.log("Accessed Middleware!");
     next();
 });
+*/
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);  // All routes defined on the routes/index.js file will be added to the app here
+app.use(routes);
 
+
+const DB_USER = 'dcrosa';
+const DB_PASSWORD = encodeURIComponent('202N81dVA0TOzVaI');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.twtrr.mongodb.net/fintrack?retryWrites=true&w=majority`,
+    ).then(() => {
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+    }).catch((err) => console.log('Não foi possível conectar ao banco!'))
