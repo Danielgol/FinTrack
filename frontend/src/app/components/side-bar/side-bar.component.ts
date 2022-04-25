1b33d25b5de203115021163a14f0546982023ac8
João Victor Cavalcanti Teixeira de Assis <joao.ctassis@gmail.com>
2022-04-25 14:42:41 -0300

    LogOut + modificações no index

--- frontend/src/app/components/side-bar/side-bar.component.ts
+++ frontend/src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faAnglesLeft, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/services/auth/auth-service.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  objectKeys = Object.keys;
+  nome: any;
+
+  constructor(private _authService: AuthService,
+              private router: Router) {}
 
   ngOnInit(): void {
+    this.nome = "Nome do user"
   }
 
+  
+  
+  logout(): void{
+    this._authService.logout();
+    this.router.navigate(['/']);
+  }
+
+
+  anglesLeft = faAnglesLeft;
 }
